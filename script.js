56da25b3a5a26f02f27930a05925e39946908de5
Juan Jose Brandani <juanjose_brandani@hotmail.com>
2023-04-25 17:58:46 -0300

    final final

--- script.js
+++ script.js
@@ -2,21 +2,17 @@ const fs = require('fs')
 
 class ProductManager {
     constructor(path) {
-        this.products = []     //para guardar en la memoria todos los productos
-        this.path = path    //para guardar en la memoria la ruta del archivo
-        this.init(path)     //para iniciar la instancia y crear el archivo en caso de no existir o cargar la memoria en caso de existir productos
+        this.products = []
+        this.path = path  
+        this.init(path)
     }
     init(path) {
-        //verifico si existe el archivo
-        let file = fs.existsSync(path)
-        //console.log(file)
-        if (!file) {
-            //si no existe lo creo
+        let file = fs.existsSync(path)        
+        if (!file) {            
             fs.writeFileSync(path,'[]')
             console.log('file created at path: '+this.path)
             return 'file created at path: '+this.path
-        } else {
-            //si existe cargo los productos en la memoria del programa
+        } else {            
             this.products = JSON.parse(fs.readFileSync(path,'UTF-8'))
             console.log('data recovered')
             return 'data recovered'
@@ -24,24 +20,16 @@ class ProductManager {
     }
     async addProduct({ title, description, price, thumbnail, code, stock }) {
         stock = stock ?? 0
-        try {
-            //defino el objeto que necesito agregar al array
-            let data = { title, description, price, thumbnail, code, stock }
-            //si la memoria tiene productos
-            if (this.products.length>0) {
-                //busco el id del último elemento y le sumo 1
-                let next_id = this.products[this.products.length-1].id+1
-                //agrego la propiedad al objeto
+        try {            
+            let data = { title, description, price, thumbnail, code, stock }            
+            if (this.products.length>0) {                
+                let next_id = this.products[this.products.length-1].id+1                
                 data.id = next_id
-            } else {
-                //en caso que no tenga: asigno el primer id
+            } else {                
                 data.id = 1
-            }
-            //agrego el objeto (usuario) a la memoria del programa
-            this.products.push(data)
-            //convierto a texto plano el array
-            let data_json = JSON.stringify(this.products,null,2)
-            //sobre-escribo el archivo
+            }            
+            this.products.push(data)            
+            let data_json = JSON.stringify(this.products,null,2)            
             await fs.promises.writeFile(this.path,data_json)
             console.log('id´s created product: '+data.id)
             return 'id´s user: '+data.id
@@ -65,19 +53,13 @@ class ProductManager {
         return null
     }
     
-    async updateProduct(id,data) {
-        //data es el objeto con las propiedades que necesito modificar del usuario
-        try {
-            //busco el usuario
-            let one = this.getProductById(id)
-            //itero para modificar la propiedad correspondiente
-            for (let prop in data) {
-                //console.log(prop)
+    async updateProduct(id,data) {        
+        try {            
+            let one = this.getProductById(id)            
+            for (let prop in data) {                
                 one[prop] = data[prop]
-            }
-            //convierto a texto plano el array
-            let data_json = JSON.stringify(this.products,null,2)
-            //sobre-escribo el archivo
+            }            
+            let data_json = JSON.stringify(this.products,null,2)            
             await fs.promises.writeFile(this.path,data_json)
             console.log('updateProduct done: '+id)
             return 'updateProduct done '+id
@@ -87,13 +69,9 @@ class ProductManager {
         }
     }
     async deleteProduct(id) {
-        try {
-            //saco el usuario
-            this.products = this.products.filter(each=>each.id!==id)
-            //console.log(this.products)
-            //convierto a texto plano el array
-            let data_json = JSON.stringify(this.products,null,2)
-            //sobre-escribo el archivo
+        try {            
+            this.products = this.products.filter(each=>each.id!==id)            
+            let data_json = JSON.stringify(this.products,null,2)            
             await fs.promises.writeFile(this.path,data_json)
             console.log('deleteProduct done: '+id)
             return 'deleteProduct done: '+id
@@ -108,7 +86,7 @@ async function manager() {
     let manager = new ProductManager('./data/data.json')
     await manager.addProduct({ title: 'producto 1', description: 'Este es un producto prueba', price: 500, thumbnail: 'Sin imagen', code:'xxx-001', stock: 12 })
     await manager.addProduct({ title: 'producto 2', description: 'Este es otro producto', price: 85, thumbnail: 'Con imagen', code:'xxx-002', stock: 15 })
-    await manager.addProduct({ title: 'producto 3', description: 'Este es un producto prueba', price: 300, thumbnail: 'Sin imagen', code:'xxx-003', stock: 43 })
+    await manager.addProduct({ title: 'producto 3', description: 'Este es un producto ', price: 300, thumbnail: 'Sin imagen', code:'xxx-003', stock: 43 })
     await manager.addProduct({ title: 'producto 4', description: 'Este es un producto', price: 40, thumbnail: 'Sin imagen', code:'xxx-004' })
     await manager.addProduct({ title: 'producto 5', description: 'Este es un producto', price: 20, thumbnail: 'Con imagen', code:'xxx-005', stock: 10 })
     await manager.addProduct({ title: 'producto 6', description: 'Este es un producto prueba', price: 50, thumbnail: 'Sin imagen', code:'xxx-006', stock: 3 })
